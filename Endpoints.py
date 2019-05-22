1b66b4570929a5c464a1845ee8cefeb8db5c9f1a
dereck05 <dereckescalante@hotmail.com>
2019-05-21 19:20:04 -0600

    eliminacion de base de datos anterior por errores

--- Endpoints.py
+++ Endpoints.py
@@ -9,7 +9,7 @@ from multiprocessing.pool import ThreadPool
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql:///usuarios'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://localhost/usuarios'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
 
--- base.pl
+++ base.pl
@@ -1,2 +1,6 @@
-comida('arroz con pollo','hola','como','estas','hoy').
-comida('frijol','adios').
+comida('arroz con pollo',['hola','soy','juan'],'como','estas','hoy').
+comida('frijol',['hola','soy','pedro'],'los','amo','todos').
+comida('pizza',['mi','nom'],'es','lechiza','botox').
+
+comidita(X,Y,Z,A,B):-comidaAux(X,C,D,F,E).
+
